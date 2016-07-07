92114701dc767d002c0917c2e958f9bca73b626a
Tom Christie <tom@tomchristie.com>
2016-07-07 10:48:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# OpenAPI Codec
+
+**An OpenAPI codec for Core API.**
+
+[![travis-image]][travis]
+[![pypi-image]][pypi]
+
+## Installation
+
+Install using pip:
+
+    $ pip install openapi-codec
+
+
+[travis-image]: https://secure.travis-ci.org/encode/openapi-codec.svg?branch=master
+[travis]: http://travis-ci.org/encode/openapi-codec?branch=master
+[pypi-image]: https://img.shields.io/pypi/v/openapi-codec.svg
+[pypi]: https://pypi.python.org/pypi/openapi-codec
---
+++ setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("__version__ = ['\"]([^'\"]+)['\"]", init_py).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+
+version = get_version('openapi_codec')
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+
+setup(
+    name='openapi-codec',
+    version=version,
+    url='http://github.com/encode/openapi-codec/',
+    license='BSD',
+    description='An OpenAPI codec for Core API.',
+    author='Tom Christie',
+    author_email='tom@tomchristie.com',
+    packages=get_packages('openapi_codec'),
+    package_data=get_package_data('openapi_codec'),
+    install_requires=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ]
+)
